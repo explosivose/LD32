dae42ae2d0da3021e3552f098ff18e8119b25ace
Matt Blickem <matt@supercore.co.uk>
2015-04-18 17:47:10 +0100

    sticky bacon

--- Assets/LD32/Scripts/Ingredient.cs
+++ Assets/LD32/Scripts/Ingredient.cs
@@ -1,6 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Ingredient : MonoBehaviour {
 	
+	public float jointBreakForce;
+	
+	public Rigidbody rb {
+		get; private set;
+	}
+	
+	void Awake() {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void OnCollisionEnter(Collision col) {
+		Ingredient other = col.gameObject.GetComponent<Ingredient>();
+		if (!other) return;
+		FixedJoint joint = gameObject.AddComponent<FixedJoint>();
+		joint.connectedBody = other.rb;
+		joint.breakForce = jointBreakForce;
+	}
 }
