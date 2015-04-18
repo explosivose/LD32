38586d43d7fc5f0f7f305c3447fb452218a1c0ef
Matt Blickem <matt@supercore.co.uk>
2015-04-18 13:34:06 +0100

    mockup of picking up and launching objects

---
+++ Assets/LD32/Scripts/Catapult.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Catapult : MonoBehaviour {
+
+	public float triggerRadius;
+	public float launchSpeed;
+
+	public void Launch() {
+		Collider[] objs = Physics.OverlapSphere(transform.position, 1f);
+		Vector3 launchDir = Vector3.forward + Vector3.up; // will calc a proper trajectory later
+		foreach (Collider c in objs) {
+			if (c.attachedRigidbody)
+			c.attachedRigidbody.AddForce(launchDir * launchSpeed, ForceMode.Impulse);
+		}
+	}
+	
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Launch();
+		}
+	}
+}
---
+++ Assets/LD32/Scripts/DragAndDrop.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Drag and drop rigidbody objects using the mouse cursor
+/// </summary>
+public class DragAndDrop : MonoBehaviour {
+
+	public float reachDistanceFromCamera;
+	public Transform counter;
+	public Pid controller;
+	
+	private Rigidbody _carrying;
+	
+	void Update () {
+		
+		// on left click start
+		if (Input.GetMouseButtonDown(0)) {
+			TryPickup();
+		}
+		if (Input.GetMouseButtonUp(0)) {
+			Drop();
+		}
+
+		
+	}
+	
+	void FixedUpdate() {
+		Carry();
+	}
+	
+	void TryPickup() {
+		Vector3 mousePos = Input.mousePosition;
+		Ray ray = Camera.main.ScreenPointToRay(mousePos);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, reachDistanceFromCamera)) {
+			if(hit.rigidbody) {
+				Pickup(hit.rigidbody);
+			}
+		}
+	}
+	
+	void Pickup(Rigidbody rb) {
+		_carrying = rb;
+		controller.Reset();
+		//_carrying.isKinematic = true;
+		_carrying.useGravity = false;
+		
+	}
+	
+	void Drop() {
+		if (_carrying) {
+			//_carrying.isKinematic = false;
+			_carrying.useGravity = true;
+			_carrying = null;
+		}
+	}
+	
+	// carry along the X/Y axis
+	void Carry() {
+		if (!_carrying)  return;
+		
+		Vector3 mousePos = Input.mousePosition;
+		mousePos.z = Mathf.Abs(Camera.main.transform.position.z - counter.position.z);
+		Vector3 target = Camera.main.ScreenToWorldPoint(mousePos);
+		//_carrying.position = Vector3.Lerp(_carrying.position, target, Time.deltaTime * 4f);
+		Vector3 direction = (target - _carrying.position).normalized;
+		float force = -controller.output(0f, direction.magnitude);
+		Debug.DrawRay(_carrying.position, direction* force);
+		_carrying.AddForce(direction * force);
+	}
+}
