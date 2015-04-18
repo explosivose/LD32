85b7a2e7dfc50da2e6c07e82f35739c24d498f02
Matt Blickem <matt@supercore.co.uk>
2015-04-18 17:06:48 +0100

    added ingredients scripts (WIP)

---
+++ Assets/LD32/Scripts/IngredientConnection.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class IngredientConnection : MonoBehaviour {
+
+	public Pid pid;
+	
+	private List<IngredientConnection> connections;
+	private Rigidbody rb;
+	
+	void Awake() {
+		connections = new List<IngredientConnection>();
+		rb = transform.GetComponentInParent<Rigidbody>();
+	}
+	
+	void FixedUpdate() {
+		foreach(IngredientConnection i in connections) {
+			Vector3 direction = i.transform.position - transform.position;
+			float force = pid.output(0f, direction.magnitude);
+			rb.AddForce(direction.normalized * force);
+		}
+	}
+	
+	void OnTriggerEnter(Collider col) {
+		IngredientConnection con = col.GetComponent<IngredientConnection>();
+		if (!con) return;
+		if (!connections.Contains(con)) connections.Add(con);
+	}
+	
+	void OnTriggerExit(Collider col) {
+		IngredientConnection con = col.GetComponent<IngredientConnection>();
+		if (!con) return;
+		connections.Remove(con);
+	}
+}
