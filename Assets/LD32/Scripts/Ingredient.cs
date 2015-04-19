f0c69a693b80896cfefab1bc5be362a5028be504
Matt Blickem <matt@supercore.co.uk>
2015-04-19 15:21:35 +0100

    added the remaining ingredient models into recipes

--- Assets/LD32/Scripts/Ingredient.cs
+++ Assets/LD32/Scripts/Ingredient.cs
@@ -1,21 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Ingredient : MonoBehaviour {
 	
 	public int score;
 	
-	public Transform instance {
-		get { return _instance; }
-		set {
-			_instance = value;
-			_instance.name = name; // removes "(Clone)" from instantiations
-		}
-	}
-	
-	private Transform _instance;
-	
-	
+		
 	void Awake() {
 		tag = "Ingredient";
 	}
