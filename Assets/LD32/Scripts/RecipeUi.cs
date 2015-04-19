115f41564529f1b83c756f6da7dd5653778fb230
Matt Blickem <matt@supercore.co.uk>
2015-04-19 17:15:21 +0100

    added a basic recipe screen

---
+++ Assets/LD32/Scripts/RecipeUi.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RecipeUi : MonoBehaviour {
+
+	public Player.Id playerId;
+	private Text text;
+	
+	private Player player;
+	
+	void Start() {
+		text = GetComponent<Text>();
+		player = Game.Instance.PlayerById(playerId);
+	} 
+	
+	void Update() {
+		text.text = player.currentRecipe.name +  " Recipe: \n";
+		foreach(GameObject ingredient in player.currentRecipe.ingredients) {
+			text.text += ingredient.name + "\n";
+		} 
+	}
+}
