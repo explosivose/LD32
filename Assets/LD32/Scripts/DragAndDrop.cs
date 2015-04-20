6c108bcd29dcc8a0f15ab4affdfc082416203c58
Matt Blickem <matt@supercore.co.uk>
2015-04-20 21:32:21 +0100

    tried to improve drag and drop control

--- Assets/LD32/Scripts/DragAndDrop.cs
+++ Assets/LD32/Scripts/DragAndDrop.cs
@@ -7,11 +7,19 @@ using System.Collections;
 public class DragAndDrop : MonoBehaviour {
 
 	public float reachDistanceFromCamera;
-
+	public Transform catapultPlatform;
+	public float jointBreakForce;
 	public float maxMoveForce;
 	public Pid controller;
 	
 	private Rigidbody _carrying;
+	private Rigidbody _attach;
+	private Vector3 _attachOffset;
+	private FixedJoint _joint;
+	
+	void Awake() {
+		_attach = transform.Find("Grabber").GetComponent<Rigidbody>();
+	}
 	
 	// called every frame
 	void Update () {
@@ -24,6 +32,16 @@ public class DragAndDrop : MonoBehaviour {
 		if (Input.GetMouseButtonUp(0)) {
 			Drop();
 		}
+
+		Vector3 mousePos = Input.mousePosition;
+		mousePos.z = Mathf.Abs(transform.position.z - catapultPlatform.position.z);
+		Vector3 target = Camera.main.ScreenToWorldPoint(mousePos);
+		Vector3 offset = _attachOffset * 1f/(Vector3.Distance(target, catapultPlatform.position)+1f);
+		_attach.transform.position = Vector3.Lerp(
+			_attach.transform.position,
+			target + offset, Time.deltaTime * 4f);
+		
+		if (!_joint) Drop();
 
 	}
 	
@@ -57,17 +75,27 @@ public class DragAndDrop : MonoBehaviour {
 				view.TransferOwnership(PhotonNetwork.player);
 			}
 		}
-		_carrying.constraints = RigidbodyConstraints.FreezePositionZ;
-		controller.Reset();
+		//_carrying.transform.position = _attach.transform.position;
+		if (!_joint) _joint = _attach.gameObject.AddComponent<FixedJoint>();
+		_joint.connectedBody = rb;
+		_joint.breakForce = jointBreakForce;
+		float zOffset = _attach.transform.position.z - _carrying.transform.position.z;
+		_attachOffset = Vector3.forward * zOffset;
+	}
+	
+	void OnJointBreak() {
+		Drop();
 	}
 	
 	// drop the rigidboy
 	void Drop() {
 		if (_carrying) {
 			//_carrying.SendMessage("Drop");
+			if (_joint) _joint.breakForce = 0f;
 			_carrying.constraints = RigidbodyConstraints.None;
 			_carrying.velocity = Vector3.zero;
 			_carrying = null;
+			_attachOffset = Vector3.zero;
 		}
 	}
 	
@@ -75,15 +103,16 @@ public class DragAndDrop : MonoBehaviour {
 	void Carry() {
 		if (!_carrying)  return;
 		
+
 		// set a carry force to follow the mouse cursor
-		Vector3 mousePos = Input.mousePosition;
-		mousePos.z = Mathf.Abs(transform.position.z - _carrying.position.z);
-		Vector3 target = Camera.main.ScreenToWorldPoint(mousePos);
+		/*
+
 		Debug.DrawLine(transform.position, target, Color.green);
 		Vector3 direction = (target - _carrying.position).normalized;
 		float force = -controller.output(0f, direction.magnitude);
 		force = Mathf.Clamp(force, 0, maxMoveForce);
 		_carrying.AddForce(direction * force);
 		Debug.DrawRay(_carrying.position, direction * force, Color.blue);
+		*/
 	}
 }
